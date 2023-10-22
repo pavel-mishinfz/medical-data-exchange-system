46b5be54e2c76bfc431a44efd175290e884038ad
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-10-22 18:40:38 +0300

    Fix methods

--- services/template-service/app/app.py
+++ services/template-service/app/app.py
@@ -1,10 +1,10 @@
 import os
 import typing
 
-from .shemas.template import Template
+from shemas import Template
 
 from fastapi import FastAPI, UploadFile
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 
 
 app = FastAPI(title='Template Service')
@@ -12,12 +12,13 @@ templates: typing.Dict[int, Template] = {}
 path_to_storage = 'C:/Medical-Data-Exchange-System/storage/'
 
 
-@app.post('/templates/', summary='Добавляет шаблон в базу')
+@app.post('/templates', summary='Добавляет шаблон в базу')
 def add_template(name: str, file: UploadFile):
+    id_file = len(templates)+1
     result = Template(
-        id=len(templates)+1,
+        id=id_file,
         name=name,
-        path=path_to_storage + file.filename
+        path=path_to_storage + str(id_file) + '.html'
     )
     templates[result.id] = result
     with open(result.path, 'w') as out_file:
@@ -26,29 +27,37 @@ def add_template(name: str, file: UploadFile):
     return result
 
 
+def generate_html_response(template_id):
+    with open(templates[template_id].path, 'r') as out_file:
+        html_content = out_file.read()
+    return HTMLResponse(content=html_content)
+
+
 @app.get('/templates/{template_id}', summary='Возвращает шаблон')
 def get_template(template_id: int):
     if template_id in templates:
-        return templates[template_id]
+        return generate_html_response(template_id)
     return JSONResponse(status_code=404, content={"message": "Шаблон не найден"})
 
 
+@app.get('/templates', summary='Возвращает список всех шаблонов')
+def get_template():
+    return [{'id': v.id, 'name': v.name} for k, v in templates.items()]
+
+
 @app.put('/templates/{template_id}', summary='Обновляет шаблон')
-def update_template(template_id: int, file: UploadFile, name: str | None = None):
+def update_template(template_id: int, name: str, file: UploadFile):
     if template_id in templates:
-        if name is None:
-            name = templates[template_id].name
         result = Template(
             id=template_id,
             name=name,
-            path=path_to_storage + file.filename
+            path=path_to_storage + str(template_id) + '.html'
         )
         with open(templates[template_id].path, 'w') as out_file:
             content = file.file.read().decode()
             out_file.write(content)
-        os.rename(templates[template_id].path, result.path)
         templates[template_id] = result
-        return result
+        return templates[template_id]
     return JSONResponse(status_code=404, content={"message": "Шаблон не найден"})
 
 
@@ -56,6 +65,7 @@ def update_template(template_id: int, file: UploadFile, name: str | None = None)
 def delete_page_template(template_id: int):
     if template_id in templates:
         os.remove(templates[template_id].path)
+        result = templates[template_id]
         del templates[template_id]
-        return JSONResponse(status_code=200, content={"message": "Шаблон успешно удален"})
+        return result
     return JSONResponse(status_code=404, content={"message": "Шаблон не найден"})
