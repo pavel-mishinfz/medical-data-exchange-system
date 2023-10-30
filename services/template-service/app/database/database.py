8f6bc59aff90e60c9d0bfe7e0f7cdaa334a99a82
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-10-30 21:09:20 +0300

    Fix database url

--- services/template-service/app/database/database.py
+++ services/template-service/app/database/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1111@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://psgadmin:localhost@/medical-system"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
