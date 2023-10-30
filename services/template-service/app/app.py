9fc86d096e7d0c41039e1f74fed5f578bf3bea1b
pavel.mishinfz <pavel.mishinfz@gmail.com>
2023-10-30 15:22:35 +0300

    Update app logic

--- services/template-service/app/app.py
+++ services/template-service/app/app.py
@@ -1,10 +1,11 @@
 import os
-import typing
-
-from .schemas import Template, TemplateBase
-
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Depends
 from fastapi.responses import JSONResponse, HTMLResponse
+from sqlalchemy.orm import Session
+from .schemas.template import Template
+from .database.database import Base, engine, SessionLocal
+from . import crud
+
 
 description = """
 Содержит _название шаблона_ и _путь к файлу_.
@@ -28,73 +29,76 @@ tags_metadata = [
     }
 ]
 
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title='Template Service',
               description=description,
               openapi_tags=tags_metadata)
-templates: typing.Dict[int, Template] = {}
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 path_to_storage = 'C:/Medical-Data-Exchange-System/storage/'
 
 
 @app.post('/templates', summary='Добавляет шаблон в базу', tags=["templates"])
-def add_template(name: str, file: UploadFile):
+def add_template(name: str, file: UploadFile, db: Session = Depends(get_db)):
     content_type = file.content_type
     if content_type == "text/html":
-        id_file = len(templates)+1
-        result = Template(
-            id=id_file,
-            name=name,
-            path=os.path.join(path_to_storage, str(id_file) + '.html')
-        )
-        templates[result.id] = result
-        with open(result.path, 'w') as out_file:
+        new_template = crud.create_template(db, name, path_to_storage)
+        with open(new_template.path, 'w') as out_file:
             content = file.file.read().decode()
             out_file.write(content)
-        return TemplateBase(id=templates[result.id].id, name=templates[result.id].name)
+        return new_template
     return JSONResponse(status_code=400, content={"message": "Недопустимый тип файла"})
 
 
-def generate_html_response(template_id):
-    with open(templates[template_id].path, 'r') as out_file:
+def generate_html_response(template: Template):
+    with open(template.path, 'r') as out_file:
         html_content = out_file.read()
     return HTMLResponse(content=html_content)
 
 
 @app.get('/templates/{template_id}', summary='Возвращает шаблон', tags=["templates"])
-def get_template(template_id: int):
-    if template_id in templates:
-        return generate_html_response(template_id)
+def get_template(template_id: int, db: Session = Depends(get_db)):
+    template = crud.get_template(db, template_id)
+    if template is not None:
+        return generate_html_response(template)
     return JSONResponse(status_code=404, content={"message": "Шаблон не найден"})
 
 
-@app.get('/templates', response_model=list[TemplateBase], summary='Возвращает список всех шаблонов', tags=["templates"])
-def get_template():
-    return [TemplateBase(id=v.id, name=v.name) for k, v in templates.items()]
+@app.get('/templates',
+         response_model=list[Template],
+         summary='Возвращает список всех шаблонов',
+         tags=["templates"])
+def get_templates(db: Session = Depends(get_db)):
+    return crud.get_templates(db)
 
 
 @app.put('/templates/{template_id}', summary='Обновляет шаблон', tags=["templates"])
-def update_template(template_id: int, name: str, file: UploadFile):
-    if template_id in templates:
+def update_template(template_id: int, name: str, file: UploadFile, db: Session = Depends(get_db)):
+    template = crud.update_template(db, template_id, name)
+    if template is not None:
         content_type = file.content_type
         if content_type == "text/html":
-            result = Template(
-                id=template_id,
-                name=name,
-                path=os.path.join(path_to_storage, str(template_id) + '.html')
-            )
-            with open(templates[template_id].path, 'w') as out_file:
+            with open(template.path, 'w') as out_file:
                 content = file.file.read().decode()
                 out_file.write(content)
-            templates[template_id] = result
-            return templates[template_id]
+            return template
         return JSONResponse(status_code=400, content={"message": "Недопустимый тип файла"})
     return JSONResponse(status_code=404, content={"message": "Шаблон не найден"})
 
 
 @app.delete('/templates/{template_id}', summary='Удаляет шаблон из базы', tags=["templates"])
-def delete_template(template_id: int):
-    if template_id in templates:
-        os.remove(templates[template_id].path)
-        result = templates[template_id]
-        del templates[template_id]
-        return TemplateBase(id=result.id, name=result.name)
+def delete_template(template_id: int, db: Session = Depends(get_db)):
+    deleted_template = crud.delete_template(db, template_id)
+    if deleted_template is not None:
+        os.remove(deleted_template.path)
+        return deleted_template
     return JSONResponse(status_code=404, content={"message": "Шаблон не найден"})
--- services/template-service/app/schemas/template.py
+++ services/template-service/app/schemas/template.py
@@ -2,9 +2,18 @@ from pydantic import BaseModel, Field
 
 
 class TemplateBase(BaseModel):
-    id: int = Field(title='Идентификатор шаблона')
+    """
+    Базовая модель шаблона
+    """
     name: str = Field(title='Название шаблона')
+    path: str = Field(title='Путь к файлу')
+
+    class ConfigDict:
+        from_attribute = True
 
 
 class Template(TemplateBase):
-    path: str = Field(title='Путь к файлу')
+    """
+    Модель используемая при запросе информации о шаблоне
+    """
+    id: int = Field(title='Идентификатор шаблона')
