9afcc6e6de0be777c14f294518a7748b5ad9e70e
Pavel Mishin <pavel.mishinfz@gmail.com>
2024-01-07 18:01:07 +0300

    Add support of loading configuration
    1) Add config.py
    2) Add .env
    3) Add database connection logic

---
+++ services/medical-card-service/app/config.py
@@ -0,0 +1,28 @@
+from typing import Tuple, Type
+from pydantic_settings import BaseSettings, SettingsConfigDict, PydanticBaseSettingsSource
+from pydantic import PostgresDsn, Field
+
+
+class Config(BaseSettings):
+    postgres_dsn: PostgresDsn = Field(
+        default='postgresql://user:pass@localhost:5432/foobar',
+        env='POSTGRES_DSN',
+        alias='POSTGRES_DSN'
+    )
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        return dotenv_settings, env_settings, init_settings
+
+
+def load_config() -> Config:
+    return Config()
