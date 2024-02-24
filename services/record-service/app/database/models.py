302d770e40c4b00fc9decb0f06e6eb5f28831f21
Pavel Mishin <pavel.mishinfz@gmail.com>
2024-02-24 13:32:08 +0300

    Fix Record model

--- services/record-service/app/database/models.py
+++ services/record-service/app/database/models.py
@@ -1,14 +1,6 @@
-from sqlalchemy import Column, Integer, CHAR, UUID, ForeignKey, Date
-from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import Column, Integer, String, UUID, Date
 
 from .database import Base
-
-
-class TimeRecord(Base):
-    __tablename__ = "time_record"
-
-    id = Column(Integer, primary_key=True, index=True)
-    time = Column(CHAR(length=10), nullable=False)
 
 
 class Record(Base):
@@ -18,6 +10,5 @@ class Record(Base):
     id_user = Column(UUID(as_uuid=True), nullable=False)
     id_doctor = Column(UUID(as_uuid=True), nullable=False)
     date = Column(Date, nullable=False)
-    id_time = mapped_column(ForeignKey('time.id'), nullable=False)
-    time = relationship(TimeRecord, uselist=False)
+    time = Column(String(length=10), nullable=False)
 
