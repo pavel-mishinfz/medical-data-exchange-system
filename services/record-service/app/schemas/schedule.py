b134884800cdcb89980061a6c9b7447933f258a8
Pavel Mishin <pavel.mishinfz@gmail.com>
2024-03-06 00:01:41 +0300

    Add schemas for Schedule

---
+++ services/record-service/app/schemas/schedule.py
@@ -0,0 +1,38 @@
+import uuid
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class ScheduleBase(BaseModel):
+    """
+    Базовая модель графика работы
+    """
+    schedule: Optional[dict] = None
+    time_per_patient: Optional[int] = None
+
+    class ConfigDict:
+        from_attribute = True
+
+
+class ScheduleIn(ScheduleBase):
+    """
+    Модель для добавления графика работы
+    """
+    id_doctor: uuid.UUID = Field(title='Идентификатор врача')
+    schedule: dict = Field(title='Дни и время работы')
+    time_per_patient: int = Field(title='Время приема одного пациента')
+
+
+class Schedule(ScheduleBase):
+    """
+    Модель используемая при запросе информации о графике работы
+    """
+    id: int = Field(title='Идентификатор графика работы')
+
+
+class ScheduleOptional(ScheduleBase):
+    """
+    Модель для обновления информации о графике работы
+    """
+    pass
