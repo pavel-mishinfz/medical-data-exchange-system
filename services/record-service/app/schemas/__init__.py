585c826c74802335c7ca90e2ccd3f57d6d2db14d
Pavel Mishin <pavel.mishinfz@gmail.com>
2024-03-06 00:01:59 +0300

    Add schemas for Schedule

--- services/record-service/app/schemas/__init__.py
+++ services/record-service/app/schemas/__init__.py
@@ -1,3 +1,5 @@
 from .record import RecordBase, RecordIn, Record, RecordOptional, RecordForUser
+from .schedule import ScheduleBase, ScheduleIn, Schedule, ScheduleOptional
 
-__all__ = [RecordBase, RecordIn, Record, RecordOptional, RecordForUser]
+__all__ = [RecordBase, RecordIn, Record, RecordOptional, RecordForUser,
+           ScheduleBase, ScheduleIn, Schedule, ScheduleOptional]
