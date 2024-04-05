bb5079fe5803ce5f57b76b6d4cd883ec9d964b21
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-04-06 00:57:06 +0300

    Delete Mail schema

--- services/user-service/app/users/schemas/mail.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class EmailBody(BaseModel):
-    to: str = Field(title='Адрес получателя')
-    subject: str = Field(title='Тема сообщения')
-    message: str = Field(title='Текст сообщения')
