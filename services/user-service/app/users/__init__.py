08cc8cd04ad1634f7fc09da798450afa7d954dbc
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-10 17:19:11 +0300

    Add crud_confirm_code and usermanager in __init__.py

--- services/user-service/app/users/__init__.py
+++ services/user-service/app/users/__init__.py
@@ -1,5 +1,5 @@
-from . import groupcrud, crud_specialization, crud_user
+from . import groupcrud, crud_specialization, crud_user, crud_confirm_code, usermanager
 from .secretprovider import inject_secrets
 from .userapp import include_routers
 
-__all__ = [include_routers, inject_secrets, groupcrud, crud_specialization, crud_user]
+__all__ = [include_routers, inject_secrets, groupcrud, crud_specialization, crud_user, crud_confirm_code, usermanager]
