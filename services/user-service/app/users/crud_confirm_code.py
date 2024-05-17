61301f037409a1dfb02e6924cf459a45f720dfc0
pavel.mishinfz <pavel.mishinfz@gmail.com>
2024-05-17 16:28:24 +0300

    Add code activation logic and update logic to get old confirmation codes

--- services/user-service/app/users/crud_confirm_code.py
+++ services/user-service/app/users/crud_confirm_code.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 import uuid
 from typing import List
 
-from sqlalchemy import delete, select, update
+from sqlalchemy import delete, select, update, and_, or_
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -22,15 +22,31 @@ async def upsert_confirm_code(session: AsyncSession,
     data = {
         "user_id": user_id,
         "code": confirm_code,
-        "create_date": datetime.now(timezone.utc)
+        "create_date": datetime.now(timezone.utc),
+        "activation_time": None
     }
     stm = insert(models.ConfirmCode).values(data)
     stm = stm.on_conflict_do_update(
         constraint='confirm_code_pkey',
-        set_={"code": data["code"], "create_date": data["create_date"]}
+        set_={"code": data["code"], "create_date": data["create_date"], "activation_time": data["activation_time"]}
     )
     result = await session.execute(stm)
 
+    await session.commit()
+    if result:
+        return await get_confirm_code(session, user_id)
+    return None
+
+
+async def activate_code(user_id: uuid.UUID, 
+                        date: datetime,
+                        session: AsyncSession) -> models.ConfirmCode:
+    """
+    Активирует код подтверждения
+    """
+    result = await session.execute(update(models.ConfirmCode)
+                                   .where(models.ConfirmCode.user_id == user_id)
+                                   .values({"activation_time": date}))
     await session.commit()
     if result:
         return await get_confirm_code(session, user_id)
@@ -47,12 +63,24 @@ async def get_confirm_code(session: AsyncSession,
 
 
 async def get_old_confirm_codes(session: AsyncSession,
-                                expired_time: datetime) -> List[models.ConfirmCode]:
+                                expired_time: datetime,
+                                expired_activation_time: datetime) -> List[models.ConfirmCode]:
     """
     Возвращает информацию об истекших кодах подтверждения
     """
     result = await session.execute(select(models.ConfirmCode)
-                                   .filter(models.ConfirmCode.create_date < expired_time))
+                                   .where(
+                                       or_(
+                                        and_(
+                                            models.ConfirmCode.create_date < expired_time,
+                                            models.ConfirmCode.activation_time == None
+                                        ),
+                                        and_(
+                                            models.ConfirmCode.activation_time != None,
+                                            models.ConfirmCode.activation_time < expired_activation_time
+                                        )
+                                       )
+                                   ))
     return result.scalars().all()
 
 
